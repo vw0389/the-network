58d2cb4f0e557f0263744748034f8433b603e286
Victor Weinert <dev@vweinert.com>
2022-01-10 14:26:39 -0600

    added dateFormatter from module

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,32 @@
 const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal);
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Reaction'
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
